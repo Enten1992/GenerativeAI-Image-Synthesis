efe0596969480acb52dcac379a86d088f667a841
Ethan Reed <grifacspirdeapath@gmx.net>
2023-04-18 00:00:00 -0400

    Initial code commit

---
+++ src/gan.py
@@ -0,0 +1,115 @@
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding=\'same\', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding=\'same\', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding=\'same\', use_bias=False, activation=\'tanh\'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding=\'same\', input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding=\'same\'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+@tf.function
+def train_step(images, generator, discriminator, generator_optimizer, discriminator_optimizer, BATCH_SIZE, noise_dim):
+    noise = tf.random.normal([BATCH_SIZE, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+def train_gan(dataset, epochs, generator, discriminator, generator_optimizer, discriminator_optimizer, BATCH_SIZE, noise_dim):
+    print("\nStarting GAN training...")
+    for epoch in range(epochs):
+        start = tf.time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch, generator, discriminator, generator_optimizer, discriminator_optimizer, BATCH_SIZE, noise_dim)
+
+        print (f\'Time for epoch {epoch + 1} is {tf.time.time()-start:.2f} sec\')
+    print("GAN training finished.")
+
+if __name__ == "__main__":
+    # Example usage with MNIST dataset
+    print("Running example GAN model with MNIST data.")
+
+    (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype(\'float32\')
+    train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
+
+    BUFFER_SIZE = 60000
+    BATCH_SIZE = 256
+
+    # Batch and shuffle the data
+    train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+    generator = make_generator_model()
+    discriminator = make_discriminator_model()
+
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    EPOCHS = 1
+    noise_dim = 100
+
+    train_gan(train_dataset, EPOCHS, generator, discriminator, generator_optimizer, discriminator_optimizer, BATCH_SIZE, noise_dim)
+
+    # Generate a sample image
+    seed = tf.random.normal([1, noise_dim])
+    predictions = generator(seed, training=False)
+    print("Generated image shape:", predictions.shape)
+    print("GAN example run successfully.")
